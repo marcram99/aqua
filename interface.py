d82a5a707cefd11becf96d858af0e0c88323d842
marcram99 <marcram99@gmail.com>
2019-02-03 15:15:49 +0100

    avancé interface

--- interface.py
+++ interface.py
@@ -1,17 +1,21 @@
 # -*-encoding:Utf-8 -*
 
 """ interface en ligne de commandes pour jeu 'aqua' """
+commande = {'HELP': [0,"tu veux de l'aide"],
+            'ADD': [2,'tu veux ajouter un cadre'],
+            'REMOVE': [1,'tu veux enleverr un cadre'],
+            'ROTATE': [2,'tu veux tourner un cadre'],
+            'INFO':[0,'tu veux des infos'],
+            }
 while True:
-    ordre = input('Que veux tu faire ?').split(' ')
-
-    if len(ordre) == 1:
-        if ordre[0] == '':
+    ordre = input('Que veux tu faire ?').upper().split(' ')
+    if len(ordre) == 1 and ordre[0] == '':
             pass
+    if len(ordre) >=1 and len(ordre) < 4:
+        if ordre[0] not in commande.keys():
+            print("j'ai pas compris...{}".format(ordre[0]))
         else:
-            print(' tu veux {}?'.format(ordre[0]))
-    elif len(ordre) == 2:
-        print(' tu veux {} et {}?'.format(ordre[0], ordre[1]))
-    elif len(ordre) == 3:
-        print(' tu veux {} et {} et {}?'.format(ordre[0], ordre[1], ordre[2]))
-    elif len(ordre) >= 4:
-        print ("trop d'arguments")
+            print("{}".format(commande[ordre[0]][1]))
+    else:
+        print('ya trop de mots là...')
+
