c9552425fc2651294f677c566a7cbe6b6e74cee2
marcram99 <marcram99@gmail.com>
2019-02-06 18:08:16 +0100

    working base interface

--- interface.py
+++ interface.py
@@ -1,21 +1,62 @@
 # -*-encoding:Utf-8 -*
+from new_logik import Cadre, Cache, Plateau
 
-""" interface en ligne de commandes pour jeu 'aqua' """
-commande = {'HELP': [0,"tu veux de l'aide"],
-            'ADD': [2,'tu veux ajouter un cadre'],
-            'REMOVE': [1,'tu veux enleverr un cadre'],
-            'ROTATE': [2,'tu veux tourner un cadre'],
-            'INFO':[0,'tu veux des infos'],
-            }
-while True:
-    ordre = input('Que veux tu faire ?').upper().split(' ')
-    if len(ordre) == 1 and ordre[0] == '':
-            pass
-    if len(ordre) >=1 and len(ordre) < 4:
-        if ordre[0] not in commande.keys():
-            print("j'ai pas compris...{}".format(ordre[0]))
+class Interpreteur():
+    """ interface en ligne de commandes pour jeu 'aqua' """
+    def __init__(self):
+        self.commandes = {'HELP': [1, "tu veux de l'aide","pas d'arguments nécéssaire à cette fonction"],
+                          'ADD': [3, 'tu veux ajouter un cache',"en arguments: cache (x1... x4) et cadre(c1... c4)"],
+                          'REMOVE': [2, 'tu veux enlever un cache',"en arguments: le cache(x1... x4)"],
+                          'ROTATE': [3, 'tu veux tourner un cache',"en arguments: le cache (x1... x4) et le sens de rotation ( H ou AH)"],
+                          'INFO':[1, 'tu veux des infos'," en argument: tout, cache, cadre "],
+                         }
+
+    def aide(self):
+        print("ceci est l'aide du jeu de la fonction")
+
+    def check_valide(self, ordre):
+        demande = [x for x in ordre if x != '' ]
+        if demande == []:
+            self.message_valide = ">pas d'arguments..."
+            valide = False
+        elif len(demande) > 3:
+            self.message_valide = ">trop d'arguments..."
+            valide = False
         else:
-            print("{}".format(commande[ordre[0]][1]))
-    else:
-        print('ya trop de mots là...')
+            if demande[0] not in self.commandes.keys():
+                self.message_valide = '>je ne comprend pas...'
+                valide = False
+            elif len(demande)  > self.commandes[demande[0]][0]:
+                self.message_valide = ">trop d'arguments pour {}".format(demande[0])
+                valide = False
+            elif len(demande)  < self.commandes[demande[0]][0]:
+                self.message_valide = ">manque arguments pour {}".format(demande[0])
+                valide = False
+            else:
+                valide = True
+                self.message_valide = self.commandes[demande[0]][1]
+        return valide
+
+    def execute(self,ordre):
+        print('>' + self.message_valide)
+        if ordre[0] == 'HELP':
+            self.aide()
+        if ordre[0] == 'ADD':
+            jeu.ajoute_cache(ordre[1].lower(),ordre[2].lower())
+        if ordre[0] == 'REMOVE':
+            jeu.enleve_cache(ordre[1].lower())
+        if ordre[0] == 'INFO':
+            jeu.info_board()
+
+
+if __name__ == '__main__':
+    jeu = Plateau()
+    i = Interpreteur()
+
+    while True:
+        ordre = input('?').upper().split(' ')
+        if i.check_valide(ordre):
+            i.execute(ordre)
+        else:
+            print(i.message_valide)
 
--- new_logik.py
+++ new_logik.py
@@ -1,8 +1,5 @@
 # -*-encoding:Utf-8 -*
 
-
-
-
 class Cadre():
     theme = ["", "salamandre", "grenouille", "poisson", "lezard", "tortue"]
     def __init__(self, liste, nom):
@@ -65,7 +62,19 @@ class Plateau():
         self.board = {'c1': self.cadre1, 'c2': self.cadre2, 'c3': self.cadre3, 'c4': self.cadre4,'x1': self.cache1, 'x2': self.cache2, 'x3': self.cache3, 'x4': self.cache4}
         self.board_state = {'c1': [], 'c2': [], 'c3': [], 'c4': [], }
         self.liste_cache_dispo = ['x1', 'x2', 'x3', 'x4']
+
         #self.term_affiche()
+
+    def animal_count(self):
+        self.animals = {x: 0 for x in Cadre.theme if x is not ''}
+        for elem in [x for x in self.board.values()]:
+            for animo in elem.liste_current:
+                if animo in self.animals:
+                    self.animals[animo] += 1
+        for key, val in self.animals.items():
+            print('{:<10} : {}'.format(key, val))
+
+
 
     def term_affiche(self):
         print("+--------------+--------------+ +--------------+--------------+")
@@ -87,42 +96,71 @@ class Plateau():
         self.info_board()
 
     def info_board(self):
-        print('-------------------------')
+        print('+-------------------------+')
         for key,val in self.board_state.items():
             if len(val) == 0:
-                info = 'Pas de cache'
+                info = 'Pas de cache    '
             else:
-                info = '{} en {}'.format(self.board[val[0]].nom,val[1])
-            print("{}: {}".format(self.board[key].nom, info))
+                info = '{} en {} '.format(self.board[val[0]].nom,val[1])
+            print("|{}: {}|".format(self.board[key].nom, info))
         l_cache = [self.board[x].nom for x in  self.liste_cache_dispo]
         l_cache.sort()
-        print('cache dispo:{}'.format(str(l_cache)[1:-1]))
+        print('+-------------------------+')
+        print('| dispo: {}'.format(str(l_cache)[1:-1]))
+        print('-----------')
+        self.animal_count()
         print('-------------------------')
 
-    def ajoute_cache(self, cache, cadre):
-        if cache in self.liste_cache_dispo:
-            if len(self.board_state[cadre]) != 0:
-                print('ADD: déjà {} sur {}'.format(self.board[self.board_state[cadre][0]].nom, self.board[cadre].nom))
-                self.enleve_cache(self.board_state[cadre][0])
-            self.board[cadre].mettre_cache(self.board[cache])
-            self.board_state[cadre] = [cache,'pos0']
-            self.liste_cache_dispo.remove(cache)
-            print('ADD: ajouté {} au {}'.format(self.board[cache].nom, self.board[cadre].nom))
-
-
+    def ajoute_cache(self, *args):
+        if args[0] not in self.board.keys():
+            print(">{} n'est pas un cache/cadre valide".format(args[0]))
+        if args[1] not in self.board.keys():
+            print(">{} n'est pas un cache/cadre valide".format(args[1]))
         else:
-            print("ADD: Le {} n'est pas disponible".format(self.board[cache].nom))
+            if args[0][:1]=='x':
+                cache = args[0]
+                cadre = args[1]
+            else:
+                cache = args[1]
+                cadre = args[0]
+            if cache in self.liste_cache_dispo:
+                if len(self.board_state[cadre]) != 0:
+                    print('>ADD: déjà {} sur {}'.format(self.board[self.board_state[cadre][0]].nom, self.board[cadre].nom))
+                    self.enleve_cache(self.board_state[cadre][0])
+                self.board[cadre].mettre_cache(self.board[cache])
+                self.board_state[cadre] = [cache,'pos0']
+                self.liste_cache_dispo.remove(cache)
+                print('>ADD: ajouté {} au {}'.format(self.board[cache].nom, self.board[cadre].nom))
 
-    def enleve_cache(self, cache):
-        if cache in self.liste_cache_dispo:
-            print("REM: Le {} n'est pas utilisé...".format(self.board[cache].nom))
+
+            else:
+                print("ADD: Le {} n'est pas disponible".format(self.board[cache].nom))
+
+    def enleve_cache(self, args):
+        if args not in self.board.keys():
+            print(">{} n'est pas un cache/cadre valide".format(args[0]))
         else:
-            liste_cadre = [x for x in self.board_state if len(self.board_state[x]) != 0]
-            cadre = [x for x in liste_cadre if self.board_state[x][0] == cache][0]
-            print('REM: {} enlevé du {}'.format(self.board[cache].nom,self.board[cadre].nom))
-            self.board[cadre].enleve_cache()
-            self.liste_cache_dispo.append(cache)
-            self.board_state[cadre] = []
+            if args[:1]=='x':
+                cache = args
+                if cache in self.liste_cache_dispo:
+                    print(">REM: Le {} n'est pas utilisé...".format(self.board[cache].nom))
+                else:
+                    liste_cadre = [x for x in self.board_state if len(self.board_state[x]) != 0]
+                    cadre = [x for x in liste_cadre if self.board_state[x][0] == cache][0]
+                    print('>REM: {} enlevé du {}'.format(self.board[cache].nom,self.board[cadre].nom))
+                    self.board[cadre].enleve_cache()
+                    self.liste_cache_dispo.append(cache)
+                    self.board_state[cadre] = []
+            if args[:1]=='c':
+                cadre = args
+                if self.board_state[cadre] == []:
+                    print(">REM: il n'y a pas de cache sur le {}".format(self.board[cadre].nom))
+                else:
+                    cache = self.board_state[cadre][0]
+                    print('>REM: {} enlevé du {}'.format(self.board[cache].nom, self.board[cadre].nom))
+                    self.board[cadre].enleve_cache()
+                    self.liste_cache_dispo.append(cache)
+                    self.board_state[cadre] = []
 
 
     def rotation_cache(self, cadre, sens):
@@ -143,8 +181,7 @@ if __name__ == '__main__':
     jeu.ajoute_cache('x4', 'c4')
     jeu.enleve_cache('x1')
     jeu.enleve_cache('x4')
-
-
     jeu.term_affiche()
+
 
 
